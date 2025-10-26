658c201c320492dfa6284eb42ea941f69d6a56d2
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-10-26 03:36:45 +0300

    Improved Standart Gun Mechanic

--- FightArene/Assets/Scripts/Guns/StandartGun.cs
+++ FightArene/Assets/Scripts/Guns/StandartGun.cs
@@ -1,3 +1,4 @@
+using System;
 using Character;
 using UnityEngine;
 using Debug = Utilities.Debug;
@@ -9,35 +10,55 @@ public class StandartGun : AGun
     [SerializeField] private float damage = 10f;
     [SerializeField] private float range = 50f;
     [SerializeField] private Transform firePoint;
-    
+
+    private Player _player;
+
+    private void Awake()
+    {
+        _player = Player.Instance;
+    }
+
     public override void Fire()
     {
         SpawnProjectile();
     }
-    
+
     private void SpawnProjectile()
     {
         Vector3 spawnPosition = firePoint != null ? firePoint.position : transform.position;
-        
+
         Vector3 targetPosition;
         var ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-        
-        if (Physics.Raycast(ray, out RaycastHit hit, range))
+
+        var raycast = Physics.Raycast(ray, out RaycastHit hit, range);
+        if (hit.collider != null && hit.collider.gameObject != _player.gameObject)
         {
             targetPosition = hit.point;
+        }
+        else if (hit.collider != null && hit.collider.gameObject != _player.gameObject)
+        {
+            Debug.Log("Hit self, ignoring.");
+            targetPosition = Vector3.zero;
         }
         else
         {
             targetPosition = ray.origin + ray.direction * range;
         }
+
+        if(targetPosition == Vector3.zero)
+        {
+            Debug.Log("No valid target found, aborting projectile spawn.");
+            return;
+        }
         
         SpawnProjectileServerRpc(spawnPosition, targetPosition);
-        
+       
         gunSource.PlayOneShot(fireSound);
     }
-    
+
     private void SpawnProjectileServerRpc(Vector3 spawnPos, Vector3 targetPos)
     {
+        
         if (Player.Instance != null)
         {
             Player.Instance.SpawnProjectileServerRpc(spawnPos, targetPos);
