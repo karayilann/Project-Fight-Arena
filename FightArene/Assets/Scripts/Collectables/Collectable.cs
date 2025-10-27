2a35a1fdd1d8c787cb5eadb7f564504f5ce38139
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-10-27 17:10:02 +0300

    Removed Collectable Type & Delay Pooling Time

--- FightArene/Assets/Scripts/Collectables/Collectable.cs
+++ FightArene/Assets/Scripts/Collectables/Collectable.cs
@@ -1,26 +1,64 @@
+using System;
 using UnityEngine;
 using Unity.Netcode;
 using Debug = Utilities.Debug;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 
 public class Collectable : PooledNetworkObject
 {
-    public CollectableType type;
+    public PoolObjectType type;
+    public float groundStayDuration = 3f;
+
+    private bool _grounded = false;
+    private CancellationTokenSource _groundCts;
 
     public override void OnSpawnFromPool()
     {
         gameObject.SetActive(true);
+        _grounded = false;
+
+        if (_groundCts != null)
+        {
+            _groundCts.Cancel();
+            _groundCts.Dispose();
+            _groundCts = null;
+        }
     }
 
     public override void OnReturnToPool()
     {
+        if (_groundCts != null)
+        {
+            _groundCts.Cancel();
+            _groundCts.Dispose();
+            _groundCts = null;
+        }
+
+        _grounded = false;
         gameObject.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Ground"))
+        if (other.gameObject.CompareTag("Ground") && !_grounded)
         {
             Debug.Log("Collectable hit the ground.");
+            _grounded = true;
+
+            _groundCts = new CancellationTokenSource();
+            HandleGroundHitAsync(_groundCts.Token).Forget();
+        }
+    }
+
+    private async UniTaskVoid HandleGroundHitAsync(CancellationToken token)
+    {
+        try
+        {
+            await UniTask.Delay((int)(groundStayDuration * 1000), cancellationToken: token);
+
+            if (token.IsCancellationRequested) return;
+
             if (IsServer)
             {
                 ReturnToPool();
@@ -30,7 +68,19 @@ public class Collectable : PooledNetworkObject
                 NotifyGroundHitServerRpc();
             }
         }
+        catch (OperationCanceledException)
+        {
+        }
+        finally
+        {
+            if (_groundCts != null)
+            {
+                _groundCts.Dispose();
+                _groundCts = null;
+            }
+        }
     }
+
 
     [ServerRpc(RequireOwnership = false)]
     private void NotifyGroundHitServerRpc(ServerRpcParams rpcParams = default)
